ce2cd7abdc58c932949b63a09da549699fdd4373
Helder Asao <helder_ha@hotmail.com>
2025-04-02 16:08:41 -0300

    alteração na seta vermelha

--- index.js
+++ index.js
@@ -7,27 +7,32 @@ console.log(setas, respostas, setasAbertas)
 setas.forEach((seta, indice) =>{
     
     seta.addEventListener('click', function(){
-        
+
+   
     setasAbertas.forEach(valor =>{
         valor.style.display='none'
-         setasAbertas[indice].style.display = 'block'     
-        
+         setasAbertas[indice].style.display = 'block'            
     })
 
-    // if(setasAbertas[indice].style.display = 'block'){
-    //     setasAbertas[indice].style.display='none'
-    //     item.style.display='none'
-    // }
       console.log(setasAbertas)
         
         respostas.forEach((item) =>{
             
             item.style.display = 'none'    
-            respostas[indice].style.display='block' 
-            
-        })
-        
+            respostas[indice].style.display='block'        
+        })  
     })
 })
+setasAbertas.forEach((setaVermelha,indice) =>{
+
+    setaVermelha.addEventListener('click', function(){
+        
+        
+        setasAbertas[indice].style.display= 'none'
+        respostas[indice].style.display = 'none'
+        
+    })
+
+ })
 
 
